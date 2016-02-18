af10077b12bfe832ff8fe144e81ad4a3cfb060c1
Mori <keitamori@gmail.com>
2016-02-18 18:33:30 +0100

    Add location to users hash

--- lib/repositories_action.rb
+++ lib/repositories_action.rb
@@ -14,18 +14,18 @@ module RepositoriesAction
     # Ok
     def get_repos_contributor_list(repo_owner, repo_name)
        res = Excon.get('https://api.github.com/repos/' + repo_owner.to_s + '/' + repo_name.to_s + '/contributors')
-       return JSON.parse(res.body)
+       res =  JSON.parse(res.body).each do |contributor|
+         contributor['location'] = search_contributor_informations(contributor['login'])['location']
+       end
+       return res
     end
+
+    private
 
     # Ok
     def search_contributor_informations(contributor_pseudo)
        res = Excon.get('https://api.github.com/users/' + contributor_pseudo.to_s)
        return JSON.parse(res.body)
-    end
-
-    # Ok
-    def find_contributor_location_coordinates(contributor_location)
-       return Geocoder.search(contributor_location.to_s)
     end
   end
 end
