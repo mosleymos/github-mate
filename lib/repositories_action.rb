7414f5d20776b6c2059eaf88b4bc783b701332c2
Mori <keitamori@gmail.com>
2016-02-18 15:22:37 +0100

    Simple basic defensive programming

--- lib/repositories_action.rb
+++ lib/repositories_action.rb
@@ -6,25 +6,26 @@ module RepositoriesAction
   class Search
     # Ok
     def search_repositories(search_term)
-       res = Excon.get('https://api.github.com/search/repositories?q='+ search_term + '+in:name'.concat('&type=Repositories'))
+       return "Aucun resultat" if search_term.to_s.nil?
+       res = Excon.get('https://api.github.com/search/repositories?q='+ search_term.to_s + '+in:name'.concat('&type=Repositories'))
        return JSON.parse(res.body)
     end
 
     # Ok
     def get_repos_contributor_list(repo_owner, repo_name)
-       res = Excon.get('https://api.github.com/repos/' + repo_owner + '/' + repo_name +'/contributors')
+       res = Excon.get('https://api.github.com/repos/' + repo_owner.to_s + '/' + repo_name.to_s + '/contributors')
        return JSON.parse(res.body)
     end
 
     # Ok
     def search_contributor_informations(contributor_pseudo)
-       res = Excon.get('https://api.github.com/users/' + contributor_pseudo)
+       res = Excon.get('https://api.github.com/users/' + contributor_pseudo.to_s)
        return JSON.parse(res.body)
     end
 
     # Ok
     def find_contributor_location_coordinates(contributor_location)
-       return Geocoder.search(contributor_location)
+       return Geocoder.search(contributor_location.to_s)
     end
   end
 end
