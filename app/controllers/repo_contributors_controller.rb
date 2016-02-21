a33dd7f43aacf41d75ff5f3b2bee5834fb38ae8a
KEITA Mori <keitamori@gmail.com>
2016-02-21 22:02:47 +0000

    Add descriptive comment on repo_contributor controller

--- app/controllers/repo_contributors_controller.rb
+++ app/controllers/repo_contributors_controller.rb
@@ -1,15 +1,18 @@
 class RepoContributorsController < ApplicationController
   include RepositoriesAction
   before_action :repo_contributor_params, :only => [:results, :show]
-
+  
+  # Homepage - search form
   def search
   end
 
+  # List of repos after a search
   def results
     @search = RepositoriesAction::Search.new
     @search_results = @search.search_repositories(params[:repo_search])
   end
 
+  # show repo informations (contributors, maps ...)
   def show
     @search_repository = RepositoriesAction::Search.new
     @repository_informations = @search_repository.get_repos_contributor_list(params[:repo_owner], params[:repo_name])
