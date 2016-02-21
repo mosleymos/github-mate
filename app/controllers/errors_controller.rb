c7ca35c49909e02b5173ea4b0da11a4cc937cc3b
KEITA Mori <keitamori@gmail.com>
2016-02-21 21:58:20 +0000

    Add descriptive comment on error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,8 +1,9 @@
 class ErrorsController < ApplicationController
+  # 404 not found page
   def not_found
     render :status => 404
   end
-
+  # 500 server error page
   def server_error
     render :status => 500
   end
