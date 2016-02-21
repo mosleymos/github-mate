35171519d90e156a22361bc94339f82019b56fbd
KEITA Mori <keitamori@gmail.com>
2016-02-21 21:56:24 +0000

    Add simple descriptive comment on method

--- lib/repositories_action.rb
+++ lib/repositories_action.rb
@@ -4,14 +4,14 @@ require 'geocoder'
 
 module RepositoriesAction
   class Search
-    # Ok
+    # search repo in github with a term
     def search_repositories(search_term)
        return "Aucun resultat" if search_term.to_s.nil?
        res = Excon.get('https://api.github.com/search/repositories?q='+ search_term.to_s + '+in:name'.concat('&type=Repositories'))
        return JSON.parse(res.body)
     end
 
-    # Ok
+    # Get list of contributor from a repo
     def get_repos_contributor_list(repo_owner, repo_name)
        res = Excon.get('https://api.github.com/repos/' + repo_owner.to_s + '/' + repo_name.to_s + '/contributors')
        res =  JSON.parse(res.body).each do |contributor|
@@ -22,7 +22,7 @@ module RepositoriesAction
 
     private
 
-    # Ok
+    # search contributor information
     def search_contributor_informations(contributor_pseudo)
        res = Excon.get('https://api.github.com/users/' + contributor_pseudo.to_s)
        return JSON.parse(res.body)
