95e6a2a7f678205b982cf58d7a6c5abb64e8e3b2
Mori <keitamori@gmail.com>
2016-02-23 17:58:30 +0100

    Contributor info functions

--- app/helpers/repo_contributors_helper.rb
+++ app/helpers/repo_contributors_helper.rb
@@ -1,6 +1,17 @@
 module RepoContributorsHelper
   def show_my_location(location=nil)
     return 'Aucune location indiquée' if location.nil?
-    return location
+    location_info = content_tag(:h5, location)
+  end
+
+  def show_my_map(location)
+  end
+
+  def show_contributor_info(contributor)
+    content_tag :div, :class => 'contributor-info' do
+        image_tag(contributor['avatar_url'], :alt => 'contributor avatar', :size => '62x62' )
+        content_tag(:h3, contributor['login'])
+        content_tag(:h5,show_my_location(contributor['location']))
+    end
   end
 end
