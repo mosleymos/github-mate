7a3cc691451f906881846e09ceeda0de3a74a559
Mori <keitamori@gmail.com>
2016-02-27 18:41:50 +0100

    Simple vcr test and installation

--- spec/lib/repositories_action_spec.rb
+++ spec/lib/repositories_action_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 require_relative '../../lib/repositories_action.rb'
-require 'pry'
+require 'typhoeus'
 
 describe 'Simple module to get informations from github api' do
   context 'Search for repos and find informations' do
@@ -9,15 +9,28 @@ describe 'Simple module to get informations from github api' do
     end
 
     it 'should search for repos with the name' do
-      expect(@search.search_repositories('Bijoux')).not_to eq nil
+      VCR.use_cassette 'lib/repository_action/search' do
+        resp = Typhoeus.get('https://api.github.com/search/repositories?q=Bijoux+in:name&type=Repositories')
+        expect(@search.search_repositories('Bijoux')).not_to eq nil
+      end
+
     end
 
     it 'should get repos contributor list' do
-      expect(@search.get_repos_contributor_list('louagej','Bijoux')).not_to eq nil
+      VCR.use_cassette 'lib/repository_action/get_contributor_list' do
+        resp = Typhoeus.get('https://api.github.com/repos/louagej/Bijoux/contributors')
+        expect(resp).not_to eq nil
+        expect(@search.get_repos_contributor_list('louagej','Bijoux')).not_to eq nil
+      end
     end
 
     it 'should search contributor information with is pseudo' do
-      expect(@search.send(:search_contributor_informations, 'Bijoux')).not_to eq nil
+      VCR.use_cassette 'lib/repository_action/contributor_information' do
+        resp = Typhoeus.get('https://api.github.com/users/louagej')
+        expect(resp).not_to eq nil
+        expect(@search.send(:search_contributor_informations, 'louagej')).not_to eq nil
+      end
+
     end
 
     it 'should concat pieces of an url' do
