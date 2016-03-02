c61dbc10da7229fc0388e1bc47bdb8e102882f54
Mori <keitamori@gmail.com>
2016-03-02 11:48:39 +0100

    Fix Heroku build

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-/bin/sh: 1: q: not found
 //= require jquery_ujs
 //= require webcomponents-lite
 //= require_tree .
